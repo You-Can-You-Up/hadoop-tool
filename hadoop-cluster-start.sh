6ed7b26913bd5e3a4f1f6023f6344124c6c3e318
haigang.sun <haigang.sun@foxmail.com>
2021-05-28 09:33:06 +0800

    feat:Add port mapping

--- hadoop-cluster-start.sh
+++ hadoop-cluster-start.sh
@@ -5,4 +5,4 @@ docker run -d --privileged -ti  --name cluster-slave1 -h slave1 --net netgroup -
 echo "容器cluster-slave1正在启动。。。"
 docker run -d --privileged -ti  --name cluster-slave2 -h slave2 --net netgroup --ip 172.15.0.4 dockerused/centos:hadoop-spark  /usr/sbin/init
 echo "容器cluster-slave1正在启动。。。"
-docker run -d --privileged -ti  --name cluster-master -h master --net netgroup --ip 172.15.0.2 dockerused/centos:hadoop-spark /usr/sbin/init 
+docker run -d --privileged -ti  --name cluster-master -h master --net netgroup --ip 172.15.0.2 -p 50070:50070 -p 8088:8088 -p 8080:8081 dockerused/centos:hadoop-spark /usr/sbin/init 
